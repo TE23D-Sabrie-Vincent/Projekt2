d733315ac38f37841ec6c04ff4c76197b95bc977
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2026-05-04 10:25:29 +0200

    Klar nu och gjorde extends / super på book (Barnklassen)för att låna egenskaper från Litteratur föräldrarklassen

--- projekt2/src/main/java/projekt/cool/Book.java
+++ projekt2/src/main/java/projekt/cool/Book.java
@@ -1,5 +1,14 @@
 package projekt.cool;
 
-public class Book {
-    
+public class Book extends Litteratur {
+    public String author;
+    public String genre;
+    public int pages;
+
+    public Book(String id, String title, boolean isAvailable, String author, String genre, int pages){
+        super(id, title, isAvailable);
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+    }
 }
