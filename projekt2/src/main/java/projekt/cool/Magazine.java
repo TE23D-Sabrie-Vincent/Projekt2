5545dc45933d62a12c59735d7bb18ab270bc1e2c
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2026-05-04 10:28:32 +0200

    Klar nu med Magazine barnklassen

--- projekt2/src/main/java/projekt/cool/Magazine.java
+++ projekt2/src/main/java/projekt/cool/Magazine.java
@@ -1,5 +1,12 @@
 package projekt.cool;
 
-public class Magazine {
-    
+public class Magazine extends Litteratur {
+    public int issueNumber;
+    public int publishedYear;
+
+    public Magazine(String id, String title, boolean isAvailable, int issueNumber, int publishedYear) {
+        super(id, title, isAvailable);
+        this.issueNumber = issueNumber;
+        this.publishedYear = publishedYear;
+    }
 }
