1dcfe66cdac3fd9c0cba56b54462856cae309ac9
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2026-05-19 21:09:47 +0200

    Ändrade med Override för att kunna sortera böcker i bokstavsordning och dessutom compareto för att göra det bokstavsordning med titel

--- projekt2/src/main/java/projekt/cool/Book.java
+++ projekt2/src/main/java/projekt/cool/Book.java
@@ -1,50 +1,77 @@
-package projekt.cool;
+import java.io.Serializable;
 
-//Barnklass som lånar egenskaper från föräldrarklassen Litteratur
-// Skillnaden är att denna har individuella egenskaper som author, genre och pages
-
-/* * Författare: Vincent sabrie 
- * Beskrivning: Underklass (barnklass) till Litteratur. Representerar en specifik bok i 
- * systemet och utökar basklassen med unika attribut för författare, genre och sidantal.
+/**
+ * Klass som representerar en enskild bok.
+ * Uppfyller C-kravet för fullständig inkapsling och sorterbarhet via
+ * Comparable.
  */
-public class Book extends Litteratur {
+public class Book implements Comparable<Book>, Serializable {
+    // Fullständig inkapsling - alla medlemsvariabler är privata
+    private String id;
+    private String title;
     private String author;
     private String genre;
     private int pages;
 
-    /*
-     * Konstruktor för att skapa ett unikt Book-objekt.
-     * Vad den gör: Skickar basdata till superklassens konstruktor via super() och
-     * initierar
-     * de bokspecifika fälten (author, genre, pages).
-     * Inparametrar: String id, String title, boolean isAvailable, String author,
-     * String genre, int pages
-     * Returvärde: Inget (Konstruktor)
-     */
-    public Book(String id, String title, boolean isAvailable, String author, String genre, int pages) {
-        // Super för att låna egenskaper från föräldrarklassen Litteratur.java
-        super(id, title, isAvailable);
+    // Konstruktor som tar emot samtliga parametrar
+    public Book(String id, String title, String author, String genre, int pages) {
+        this.id = id;
+        this.title = title;
         this.author = author;
         this.genre = genre;
         this.pages = pages;
     }
 
-    /*
-      Vad metoden gör: Returnerar namnet på bokens författare.
-      Inparametrar: Inga
-      Returvärde: String (Författarens namn)
-     */
+    // Getters och Setters för samtliga fält (Inkapsling)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getAuthor() {
         return author;
     }
 
-    /*
-      Vad metoden gör: Hämtar och returnerar bokens totala antal sidor.
-      Inparametrar: Inga
-      Returvärde: int (Antal sidor)
-     */
-    public int getPages() {
-        return this.pages;
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    // Sorterar böcker i bokstavsordning baserat på titel
+    @Override
+    public int compareTo(Book other) {
+        return this.title.compareToIgnoreCase(other.title);
+    }
+
+    // Använder alla fält i utskriften
+    @Override
+    public String toString() {
+        return "[ID: " + id + "] " + title + " av " + author + " (" + genre + ", " + pages + " sidor)";
+    }
 }
